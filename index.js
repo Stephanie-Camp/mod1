d6679e5404fd3f0f018db5bf3ba13d2d74a2e6a7
Stephanie-Camp <18564239-Stephanie-Camp@users.noreply.replit.com>
2023-06-12 02:17:07 +0000

    Initial Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var numero1 = 21;
+var numero2 = 7;
+var soma = numero1 + numero2;
+var subtracao = numero1 - numero2;
+var multiplicacao = numero1 * numero2;
+var divisao = numero1/numero2;
+var resto = 0;
+
+var nome = "Stephanie";
+var idade = 20;
+var n = "Olá!\nMeu nome é " + nome + ", e esse ano cheguei na casa dos " + idade;
+
+var maiorDeIdade = 20 > 18;
+
+var peso = 50.5;
+var altura = 1.64;
+var imc = peso/(altura**2);
+
+var resultado = soma + subtracao * multiplicacao / divisao;
+
