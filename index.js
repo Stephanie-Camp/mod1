0cc8330e5d94e1a6be2b6894c8895fd526061db6
Stephanie-Camp <18564239-Stephanie-Camp@users.noreply.replit.com>
2023-06-13 22:51:53 +0000

    Primeira atividade

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 var numero1 = 21;
 var numero2 = 7;
+
 var soma = numero1 + numero2;
+console.log(soma);
+
 var subtracao = numero1 - numero2;
+console.log(subtracao);
+
 var multiplicacao = numero1 * numero2;
+console.log(multiplicacao);
+
 var divisao = numero1/numero2;
-var resto = 0;
+console.log(divisao);
+
+var resto = numero1%numero2;
+console.log(resto);
 
 var nome = "Stephanie";
 var idade = 20;
-var n = "Olá!\nMeu nome é " + nome + ", e esse ano cheguei na casa dos " + idade;
+console.log("Olá!\nMeu nome é " + nome + ", e esse ano cheguei na casa dos " + idade);
 
 var maiorDeIdade = 20 > 18;
+console.log(maiorDeIdade);
 
 var peso = 50.5;
 var altura = 1.64;
 var imc = peso/(altura**2);
+console.log(imc);
 
 var resultado = soma + subtracao * multiplicacao / divisao;
+console.log(resultado);
 
